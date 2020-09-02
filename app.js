70e533310a38155d018fad15f8fddc3adcd94a45
Leandro <leandrosf934@gmail.com>
2020-09-02 13:10:50 -0300

    Atualização do dotenv

--- app.js
+++ app.js
@@ -2,18 +2,14 @@ import expres from 'express';
 import { accountRouter } from './route/accountRouter.js';
 import mongoose from 'mongoose';
 
-require('dotenv').config();
+const app = expres();
 
 async function conectMongo() {
   try {
-    await mongoose.connect(
-      `mongodb+srv://${process.env.USERDB}:${process.env.PWDDB}
-        @cluster0.m3izc.mongodb.net/bank?retryWrites=true&w=majority`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    await mongoose.connect(process.env.MONGODB_KEY, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
     console.log(`Banco de dados: Conectado!`);
   } catch (err) {
@@ -24,11 +20,10 @@ async function conectMongo() {
 
 conectMongo();
 
-const app = expres();
 app.use(expres.json());
 app.use(accountRouter);
 
-app.listen(process.env.PORT, async () => {
+app.listen(process.env.PORT_LOCAL, async () => {
   try {
     console.log('API Iniciada');
   } catch (err) {
